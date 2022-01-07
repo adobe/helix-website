df31309ef3d60bf8976bd6e341b861f3d3b06561
David Nuescheler <david@nuescheler.ch>
2022-01-07 14:46:39 -0800

    chore: don't block for service status

--- blocks/service-status/service-status.js
+++ blocks/service-status/service-status.js
@@ -1,8 +1,14 @@
 import { loadScript } from '../../scripts/scripts.js';
 import createTag from '../../utils/tag.js';
 
-export default async function init(el) {
+async function loadWidget(el) {
   await loadScript('https://unpkg.com/@statuspage/status-widget/dist/index.js');
   const status = createTag('statuspage-widget', { src: 'https://status.project-helix.io', appearance: 'badge' });
   el.append(status);
+}
+
+export default async function init(el) {
+  setTimeout(() => {
+    loadWidget(el)
+  }, 3000);
 }
