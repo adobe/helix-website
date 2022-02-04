f5a8353cf0dab30db6c70fe0c212416bf516d6c9
rofe <github@rofe.com>
2022-02-04 21:03:43 +0100

    feat: auto-enumeration

--- blocks/footnotes/footnotes.js
+++ blocks/footnotes/footnotes.js
@@ -1,11 +1,19 @@
 export default async function decorate($block) {
+  // enumerate all footnote references
+  [...document.querySelectorAll('main sup')]
+    .forEach((sup, i) => {
+      if (sup.textContent === '#') {
+        sup.textContent = i + 1;
+      }
+    });
+
+  // enumerate footnotes
   document.querySelector('footer').append($block);
-  [...$block.children].forEach((footnote) => {
-    if (footnote.children.length === 2) {
-      const $num = footnote.children[0];
-      $num.classList.add('footnote-num');
-      $num.innerHTML = `<sup>${$num.textContent}</sup>`;
-      $num.nextElementSibling.classList.add('footnote-text');
-    }
+  [...$block.children].forEach(($footnote, i) => {
+    $footnote.firstElementChild.classList.add('footnote-text');
+    const $num = document.createElement('div');
+    $num.classList.add('footnote-num');
+    $num.innerHTML = `<sup>${i + 1}</sup>`;
+    $footnote.prepend($num);
   });
 }
