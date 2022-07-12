22285d3365ce3a03e4c3a2d9ddfd2a4e98154800
David Nuescheler <david@nuescheler.ch>
2022-07-12 14:47:41 -0700

    feat: simple video block

---
+++ blocks/video/video.js
@@ -0,0 +1,25 @@
+function decorateVideoBlock($block, videoURL) {
+  if (videoURL.endsWith('.mp4')) {
+    let attrs = '';
+    attrs = 'playsinline controls';
+    if ($block.classList.contains('autoplay')) attrs = 'playsinline controls muted autoplay loop';
+    $block.innerHTML = /* html */`
+      <div class="vid-wrapper">
+        <video ${attrs} name="media"><source src="${videoURL}" type="video/mp4"></video>
+      </div>
+      `;
+  }
+}
+
+export default function decorate($block) {
+  const $a = $block.querySelector('a');
+  const videoURL = $a.href;
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        decorateVideoBlock($block, videoURL);
+      }
+    });
+  });
+  observer.observe($block);
+}
