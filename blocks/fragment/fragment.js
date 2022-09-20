73f0ef16d3529dfa30f4a9f09754062809d96602
David Nuescheler <david@nuescheler.ch>
2022-09-20 10:29:26 -0600

    chore: update fragment block

--- blocks/fragment/fragment.js
+++ blocks/fragment/fragment.js
@@ -1,17 +1,41 @@
-async function fetchFragment(path) {
-  const resp = await fetch(`${path}.plain.html`);
-  if (resp.ok) {
-    return resp.text();
+/*
+ * Fragment Block
+ * Include content from one Helix page in another.
+ * https://www.hlx.live/developer/block-collection/fragment
+ */
+
+import {
+  decorateMain,
+  loadBlocks,
+} from '../../scripts/scripts.js';
+
+/**
+ * Loads a fragment.
+ * @param {string} path The path to the fragment
+ * @returns {HTMLElement} The root element of the fragment
+ */
+async function loadFragment(path) {
+  if (path && path.startsWith('/')) {
+    const resp = await fetch(`${path}.plain.html`);
+    if (resp.ok) {
+      const main = document.createElement('main');
+      main.innerHTML = await resp.text();
+      decorateMain(main);
+      await loadBlocks(main);
+      return main;
+    }
   }
   return null;
 }
 
-export default async function init(element) {
-  const path = element.querySelector('div > div').textContent;
-  const html = await fetchFragment(path);
-  if (!html) return null;
-  element.insertAdjacentHTML('beforeend', html);
-  element.querySelector('div').remove();
-  element.classList.add('is-Visible');
-  return element;
+export default async function decorate(block) {
+  const link = block.querySelector('a');
+  const path = link ? link.getAttribute('href') : block.textContent.trim();
+  const fragment = await loadFragment(path);
+  if (fragment) {
+    const fragmentSection = fragment.querySelector(':scope .section');
+    if (fragmentSection) {
+      block.closest('.fragment-wrapper').replaceWith(...fragmentSection.childNodes);
+    }
+  }
 }
