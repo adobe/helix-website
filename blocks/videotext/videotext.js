bdc0eca08c2d0f62a8730307ea496f3383308918
Brian Warner <3vil3mpir3@gmail.com>
2022-10-27 15:12:41 -0400

    reverting js change

--- blocks/videotext/videotext.js
+++ blocks/videotext/videotext.js
@@ -1,14 +1,14 @@
-export default function decorate($block) {
-  [...$block.children].forEach((row) => {
-    const $videoEl = [...row.children][0];
-    $videoEl.classList.add('videocontent');
+export default function decorate(block) {
+  [...block.children].forEach((row) => {
+    const videoEl = [...row.children][0];
+    videoEl.classList.add('videocontent');
     const videoURL = videoEl.innerHTML;
-    $videoEl.innerHTML = /* html */
+    videoEl.innerHTML = /* html */
     `<video playsinline controls name="media"><source src="${videoURL}" type="video/mp4"></video>`;
-    const $textEl = [...row.children][1];
-    $textEl.classList.add('text');
-    $textEl.querySelectorAll('a').forEach(($a) => {
-      $a.target="_blank";
+    const textEl = [...row.children][1];
+    textEl.classList.add('text');
+    textEl.querySelectorAll('a').forEach((a) => {
+      a.target="_blank";
     });
   });
 }
