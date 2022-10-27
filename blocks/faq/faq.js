144d93dd9a98d557c1d45b81f715e4bf3360f3f3
Kunwar <saluja@adobe.com>
2022-10-27 12:18:20 -0400

    FAQ block updates

--- blocks/faq/faq.js
+++ blocks/faq/faq.js
@@ -9,12 +9,15 @@ export default async function decorateFaq($block) {
   const json = await resp.json();
   $block.innerText = '';
   const $dl = document.createElement('dl');
+  $dl.className='faq-accordion';
   json.data.forEach((row, i) => {
     const $dt = document.createElement('dt');
+    $dt.className='faq-question';
     const $a = document.createElement('a');
     $a.id = `q${(i + 1)}`;
     $dt.innerText = row.Question;
     const $dd = document.createElement('dd');
+    $dd.className='faq-answer';
     const answer = autoLink(row.Answer);
 
     $dd.innerHTML = answer;
@@ -27,4 +30,22 @@ export default async function decorateFaq($block) {
   if (selected) {
     selected.scrollIntoView();
   }
+
+
+
+var acc = document.getElementsByClassName("faq-question");
+console.log(acc.length);
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}   
 }
