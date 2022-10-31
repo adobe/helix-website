05c4b3ddd926b02bc5060f1e9bd4b1c8578003a0
vmitra <vmitra@adobe.com>
2022-10-31 10:45:24 -0400

    fixed linting errors

--- blocks/videotext/videotext.js
+++ blocks/videotext/videotext.js
@@ -3,12 +3,11 @@ export default function decorate(block) {
     const videoEl = [...row.children][0];
     videoEl.classList.add('videocontent');
     const videoURL = videoEl.innerHTML;
-    videoEl.innerHTML = /* html */
-    `<video playsinline controls name="media"><source src="${videoURL}" type="video/mp4"></video>`;
+    videoEl.innerHTML = `<video playsinline controls name="media"><source src="${videoURL}" type="video/mp4"></video>`;
     const textEl = [...row.children][1];
     textEl.classList.add('text');
     textEl.querySelectorAll('a').forEach((a) => {
-      a.target="_blank";
+      a.target = '_blank';
     });
   });
 }
