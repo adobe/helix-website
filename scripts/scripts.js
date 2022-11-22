23215895281837c0b994a7e227e90f10f5d83b86
Kunwar <saluja@adobe.com>
2022-11-22 16:16:56 -0500

    Tab block
    Block to introduce Tabs UI to include videos and text based tutorial

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -79,6 +79,23 @@ export function loadCSS(href, callback) {
   }
 }
 
+export function createTag(tag, attributes, html) {
+  const el = document.createElement(tag);
+  if (html) {
+    if (html instanceof HTMLElement || html instanceof SVGElement) {
+      el.append(html);
+    } else {
+      el.insertAdjacentHTML('beforeend', html);
+    }
+  }
+  if (attributes) {
+    Object.entries(attributes).forEach(([key, val]) => {
+      el.setAttribute(key, val);
+    });
+  }
+  return el;
+}
+
 /**
  * Retrieves the content of a metadata tag.
  * @param {string} name The metadata name (or property)
