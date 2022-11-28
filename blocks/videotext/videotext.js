a6d5302bed14d349683e294f156e7b9523264b31
vmitra <vmitra@adobe.com>
2022-11-28 18:51:04 -0500

    Update videotext.js

--- blocks/videotext/videotext.js
+++ blocks/videotext/videotext.js
@@ -3,7 +3,7 @@ export default function decorate(block) {
     const videoEl = [...row.children][0];
     videoEl.classList.add('videocontent');
     const videoURL = videoEl.innerHTML;
-    videoEl.innerHTML = `<video preload="none" playsinline controls name="media"><source src="${videoURL}" type="video/mp4"></video>`;
+    videoEl.innerHTML = `<video playsinline controls name="media"><source src="${videoURL}" type="video/mp4"></video>`;
     const textEl = [...row.children][1];
     textEl.classList.add('text');
     textEl.querySelectorAll('a').forEach((a) => {
