df7c5e912bbe48abd88db5f2585a493b938935f7
rofe <github@rofe.com>
2023-04-21 00:00:28 +0200

    feat: faq anchor links

--- blocks/faq/faq.js
+++ blocks/faq/faq.js
@@ -1,3 +1,5 @@
+import { addAnchorLink } from '../../scripts/scripts.js';
+
 function autoLink(string) {
   const pattern = /(^|[\s\n]|<[A-Za-z]*\/?>)((?:https?):\/\/[-A-Z0-9+\u0026\u2019@#/%?=()~_|!:,.;]*[-A-Z0-9+\u0026@#/%=~()_|])/gi;
   return string.replace(pattern, '$1<a href="$2">$2</a>');
@@ -11,15 +13,14 @@ export default async function decorateFaq($block) {
   const $dl = document.createElement('dl');
   json.data.forEach((row, i) => {
     const $dt = document.createElement('dt');
-    const $a = document.createElement('a');
-    $a.id = row.Id || `q${(i + 1)}`;
+    $dt.id = row.Id || `q${(i + 1)}`;
     $dt.innerText = row.Question;
+    addAnchorLink($dt);
     const $dd = document.createElement('dd');
     const answer = autoLink(row.Answer);
 
     $dd.innerHTML = answer;
-    $a.append($dt, $dd);
-    $dl.append($a);
+    $dl.append($dt, $dd);
   });
   $block.append($dl);
 
