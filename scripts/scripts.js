f10d261600df3cb317652c4bd08ea05959b5ac9c
rofe <github@rofe.com>
2023-05-12 10:49:54 +0200

    chore: favicon update

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -682,7 +682,7 @@ async function loadLazy(doc) {
   }
 
   loadCSS('/fonts/fonts.css');
-  addFavIcon(`${window.hlx.codeBasePath}/img/icon-aeg.svg`);
+  addFavIcon(`${window.hlx.codeBasePath}/img/icon-aec.svg`);
 
   if (getMetadata('supressframe')) {
     doc.querySelector('header').remove();
