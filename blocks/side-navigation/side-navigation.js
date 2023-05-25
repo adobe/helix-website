d229bc5737abe2f3ba9f890ee809ea26dabd5a17
Nicole Rego <nicole.rego@thinkingbox.com>
2023-05-25 15:24:13 -0400

    [#230] block side navigation - ux mobile

--- blocks/side-navigation/side-navigation.js
+++ blocks/side-navigation/side-navigation.js
@@ -1,4 +1,21 @@
+import createTag from '../../utils/tag.js';
+
 export default function decorate(block) {
+  const docBtnInner = '<button>📖 Docs</button>';
+  const docButton = createTag('div', { class: 'side-navigation-overlay-btn-wrapper' }, docBtnInner);
+
+  const backBtnInner = '<button>← Back</button>';
+  const backBtn = createTag('div', { class: 'side-navigation-overlay-btn-wrapper' }, backBtnInner);
+
+  block.parentElement.prepend(docButton);
+  block.prepend(backBtn);
+
+  [docButton, backBtn].forEach((btn) => {
+    btn.addEventListener('click', () => {
+      block.classList.toggle('overlay');
+    });
+  });
+
   block.querySelectorAll(':scope > div > div > ul > li').forEach((list) => {
     list.classList.add('list-section');
     list.querySelectorAll(':scope > ul').forEach((listInner) => {
