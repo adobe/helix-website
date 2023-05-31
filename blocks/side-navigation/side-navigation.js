630c2ceb734f946d87e688d81dbff02b9c68ed0a
Nicole Rego <nicole.rego@thinkingbox.com>
2023-05-31 14:55:59 -0400

    [#0] Side Navigation block - mobile layout

--- blocks/side-navigation/side-navigation.js
+++ blocks/side-navigation/side-navigation.js
@@ -77,10 +77,16 @@ export default function decorate(block) {
   });
 
   block.querySelectorAll(':scope > div > div > ul > li').forEach((list) => {
-    list.classList.add('list-section');
+    list.classList.add('list-section', 'closed');
+
+    list.addEventListener('click', (e) => {
+      e.target.classList.toggle('closed');
+    });
+
     list.querySelector(':scope > a').classList.add('heading');
     list.querySelectorAll(':scope > ul').forEach((listInner) => {
       listInner.classList.add('list-section-inner');
+      listInner.classList.add('closed');
 
       listInner.querySelectorAll(':scope > li > ul').forEach((nestedList) => {
         const listTarget = nestedList.parentElement;
