55cf6f963533d8d8f3df5ad0fd3d76c9a64be8ee
snowiewdev <snowiewongdev@gmail.com>
2023-06-05 12:51:23 -0700

    [#0] updates for improving SEO score

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -4,7 +4,7 @@ import {
 import { getEnv } from '../../utils/env.js';
 import createTag from '../../utils/tag.js';
 
-const BRAND_IMG = '<img loading="lazy" alt="Adobe" src="/blocks/header/adobe-logo.svg">';
+const BRAND_IMG = '<img loading="lazy" alt="Adobe" width="25" height="22.12" src="/blocks/header/adobe-logo.svg">';
 const SEARCH_ICON = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" focusable="false">
 <path d="M14 2A8 8 0 0 0 7.4 14.5L2.4 19.4a1.5 1.5 0 0 0 2.1 2.1L9.5 16.6A8 8 0 1 0 14 2Zm0 14.1A6.1 6.1 0 1 1 20.1 10 6.1 6.1 0 0 1 14 16.1Z"></path>
 </svg>`;
