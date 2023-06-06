cfc52096e74eb71402c4bfda1e3f61fdae2666f5
snowiewdev <snowiewongdev@gmail.com>
2023-06-06 10:55:15 -0700

    [#254] increase pagination tap area & style updates

--- blocks/pagination/pagination.js
+++ blocks/pagination/pagination.js
@@ -2,5 +2,10 @@ export default function decorate(block) {
   block.querySelectorAll(':scope > div > div').forEach((column, i) => {
     column.classList.add('column');
     column.classList.add(i % 2 ? 'right' : 'left');
+
+    column.addEventListener('click', () => {
+      const linkElement = column.querySelector('a');
+      if (linkElement) linkElement.click();
+    });
   });
 }
