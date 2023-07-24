fcb2da0839e5ea4dd725f7925df0e1cde57db6a5
snowiewdev <snowiewongdev@gmail.com>
2023-07-24 11:33:29 -0700

    test for breadcrumb perf fix - fixed height

--- blocks/breadcrumb/breadcrumb.js
+++ blocks/breadcrumb/breadcrumb.js
@@ -57,6 +57,4 @@ export default async function decorate(block) {
 
   const innerDiv = block.querySelector(':scope > div > div');
   innerDiv.append(list);
-
-  block.closest('.breadcrumb').classList.add('appear');
 }
