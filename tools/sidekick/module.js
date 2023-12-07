b7bf83e46f9dea702856de7491a1c561f8f9926a
rofe <rofe@users.noreply.github.com>
2023-12-07 12:37:44 +0000

    chore(sidekick): new release candidate [skip ci]

--- tools/sidekick/module.js
+++ tools/sidekick/module.js
@@ -374,10 +374,14 @@ import sampleRUM from './rum.js';
     if (baseHost === host) {
       return true;
     }
-    // matching project domains
-    const projectDomains = ['.aem.page', '.aem.live', '.hlx.page', '.hlx.live'];
-    if (!projectDomains.find((domain) => baseHost.endsWith(domain)
-      && host.endsWith(domain))) {
+    // check for matching domain suffixes
+    const previewSuffixes = ['.aem.page', '.hlx.page'];
+    const liveSuffixes = ['.aem.live', '.hlx.live'];
+    const isPreview = previewSuffixes.some((suffix) => baseHost.endsWith(suffix))
+      && previewSuffixes.some((suffix) => host.endsWith(suffix));
+    const isLive = liveSuffixes.some((suffix) => baseHost.endsWith(suffix))
+      && liveSuffixes.some((suffix) => host.endsWith(suffix));
+    if (!isPreview && !isLive) {
       return false;
     }
     // project details
@@ -2180,10 +2184,9 @@ import sampleRUM from './rum.js';
                     // open url in new window
                     window.open(target, `hlx-sk-${id || `custom-plugin-${i}`}`);
                   }
-                } else if (eventName) {
-                  // fire custom event
-                  fireEvent(sk, `custom:${eventName}`);
                 }
+                // fire custom event
+                fireEvent(sk, `custom:${eventName || id}`);
               },
               isDropdown: isContainer,
             },
