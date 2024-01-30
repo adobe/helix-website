e51d956c091fee72f8580b8d1d8f80ffca2f0045
Bertrand Delacretaz <bdelacretaz@apache.org>
2024-01-30 16:05:12 +0100

    fix: lazy-loader tests

---
+++ tests/web-components/lazy-loader.test.js
@@ -0,0 +1,37 @@
+/* eslint-disable no-unused-expressions */
+/* global describe it */
+
+import { readFile } from '@web/test-runner-commands';
+import { expect } from '@esm-bundle/chai';
+import '../../web-components/lazy-loader.js';
+
+document.body.innerHTML = await readFile({ path: './lazy-loader.mock.html' });
+
+describe('Lazy-loader Web Component test', async () => {
+  const sceSelector = 'some-custom-element';
+  const scriptSelector = "script[src='42']";
+
+  it('Registers a lazy-loader component', async () => {
+    const loader = customElements.get('lazy-loader');
+    expect(loader).to.exist;
+  });
+
+  it('Does not activate elements before hlx:delayed ', async () => {
+    expect(document.body.querySelector(sceSelector)).to.be.null;
+    expect(document.head.querySelector(scriptSelector)).to.be.null;
+  });
+
+  it('Activates elements once hlx:delayed is received', async () => {
+    document.dispatchEvent(new CustomEvent('hlx:delayed'));
+    const sce = document.body.querySelector(sceSelector);
+    expect(sce).to.exist;
+    expect(sce.getAttribute('id')).to.equal('12');
+  });
+
+  it('Moves scripts under document.head', async () => {
+    document.dispatchEvent(new CustomEvent('hlx:delayed'));
+    const script = document.head.querySelector(scriptSelector);
+    expect(script).to.exist;
+    expect(script.getAttribute('type')).to.equal('test-type');
+  });
+});
--- web-components/lazy-loader.js
+++ web-components/lazy-loader.js
@@ -1,16 +1,20 @@
 // Web Component that activates any <template>
-// elements that it contains only once the 
+// elements that it contains only once the
 // 'hlx:delayed' event is received.
 class LazyLoader extends HTMLElement {
+  static register() {
+    customElements.define('lazy-loader', LazyLoader);
+  }
+
   constructor() {
     super();
     document.addEventListener('hlx:delayed', this.activateTemplates.bind(this));
   }
 
   activateTemplates() {
-    this.querySelectorAll('template').forEach(t => {
+    this.querySelectorAll('template').forEach((t) => {
       this.append(t.content.cloneNode(true));
-      this.querySelectorAll('script').forEach(s => {
+      this.querySelectorAll('script').forEach((s) => {
         // move any script tags found in the template
         // to document.head to activate them
         const script = document.createElement('script');
@@ -18,13 +22,13 @@ class LazyLoader extends HTMLElement {
         script.setAttribute('type', s.getAttribute('type'));
         document.head.appendChild(script);
         s.parentNode.removeChild(s);
-      })
+      });
       // Remove any "please wait" elements
-      this.querySelectorAll('.remove').forEach(r => {
+      this.querySelectorAll('.remove').forEach((r) => {
         r.parentNode.removeChild(r);
-      })
-    })
+      });
+    });
   }
 }
 
-customElements.define('lazy-loader', LazyLoader);
+LazyLoader.register();
