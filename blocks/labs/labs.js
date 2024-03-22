c0eab09d83dcb54c6b875c4e06d8adcdfc9c2b13
Lars Trieloff <lars@trieloff.net>
2024-03-22 15:55:23 +0100

    fix(labs): better brand new styling

--- blocks/labs/labs.js
+++ blocks/labs/labs.js
@@ -1,11 +1,11 @@
 export default function decorate(block) {
-  const headings = block.querySelector('h1, h2, h3, h4, h5, h6');
-  const body = block.querySelector('p');
   const header = document.createElement('div');
   header.className = 'header';
-  header.textContent = headings.textContent || 'Early-access technology';
-  block.innerHTML = body
-    ? body.innerHTML
-    : `Ask us about this feature from the ${block.textContent.trim()} labs on your Slack channel!`;
+  header.textContent = block.textContent.startsWith('Brand New')
+    ? 'Brand New'
+    : 'Early-access technology';
+  block.innerHTML = block.textContent.startsWith('Brand New')
+    ? block.textContent.replace('Brand New', '').trim()
+    : ` Ask us about this feature from the ${block.textContent.trim()} labs on your Slack channel!`;
   block.prepend(header);
 }
