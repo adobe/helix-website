b17fe4510e68669b279621a7c9c1a80c6c4d508c
Lars Trieloff <lars@trieloff.net>
2024-03-22 15:45:02 +0100

    feat(labs): allow custom labs headers

--- blocks/labs/labs.js
+++ blocks/labs/labs.js
@@ -1,7 +1,11 @@
 export default function decorate(block) {
+  const headings = block.querySelector('h1, h2, h3, h4, h5, h6');
+  const body = block.querySelector('p');
   const header = document.createElement('div');
   header.className = 'header';
-  header.textContent = 'Early-access technology';
-  block.innerHTML = `Ask us about this feature from the ${block.textContent.trim()} labs on your Slack channel!`;
+  header.textContent = headings.textContent || 'Early-access technology';
+  block.innerHTML = body
+    ? body.innerHTML
+    : `Ask us about this feature from the ${block.textContent.trim()} labs on your Slack channel!`;
   block.prepend(header);
 }
