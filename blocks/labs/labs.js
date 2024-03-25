e0bbe66c4befad0e6a0fc8bd15e12407ba65b62f
Lars Trieloff <lars@trieloff.net>
2024-03-25 10:26:23 +0100

    fix(labs): use colon as a delimiter

--- blocks/labs/labs.js
+++ blocks/labs/labs.js
@@ -1,11 +1,11 @@
 export default function decorate(block) {
   const header = document.createElement('div');
   header.className = 'header';
-  header.textContent = block.textContent.startsWith('Brand New')
-    ? 'Brand New'
-    : 'Early-access technology';
-  block.innerHTML = block.textContent.startsWith('Brand New')
-    ? block.textContent.replace('Brand New', '').trim()
-    : ` Ask us about this feature from the ${block.textContent.trim()} labs on your Slack channel!`;
+  const parts = block.innerText.split(/:/);
+  if (parts.length === 1) {
+    parts.unshift('Early-access technology');
+    parts[1] = ` Ask us about this feature from the ${parts[1]} labs on your Slack channel!`;
+  }
+  [header.textContent, block.innerHTML] = parts;
   block.prepend(header);
 }
