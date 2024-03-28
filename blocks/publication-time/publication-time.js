64e6c07516da92c1e3fc31be7b7eeced7b526626
Bertrand Delacretaz <bdelacretaz@apache.org>
2024-03-28 18:31:13 +0100

    feat:publication-time block

---
+++ blocks/publication-time/publication-time.js
@@ -0,0 +1,21 @@
+import '../../web-components/relative-time.js';
+
+export default function decorate($block) {
+  // First row of the block defines the text prefix
+  const prefix = $block.querySelector(':scope > div > div ')?.textContent?.trim();
+
+  // Output the <relative-time> element as specified,
+  // https://github.com/github/relative-time-element
+  if(!document.lastModified) {
+    console.error('document.lastModified returns null');
+  } else {
+    const lastMod = new Date(document.lastModified);
+    const p = document.createElement('p');
+    const rt = document.createElement('relative-time');
+    rt.setAttribute('datetime', lastMod.toISOString())
+    rt.textContent = lastMod.toLocaleDateString();
+    p.textContent = prefix + ' ';
+    p.appendChild(rt);
+    $block.replaceChildren(p);
+  }
+}
