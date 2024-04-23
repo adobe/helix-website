527f0d84667d0f550e6921dcf9a2cc652108ca78
Alexandre Capt <acapt@adobe.com>
2024-04-23 15:36:06 +0200

    feat: minimum of styling

--- blocks/venn/venn.js
+++ blocks/venn/venn.js
@@ -1,35 +1,4 @@
 
-// const draw = (placeholder, d) => {
-//   d.sets.forEach((set) => {
-//     const container = document.createElement('div');
-//     placeholder.appendChild(container);
-
-//     container.appendChild(document.createElement('span'));
-//     container.appendChild(document.createElement('span'));
-
-//     if (set.sets.length === 1) {
-//       container.classList.add('circle');
-//     } else {
-//       container.classList.add('shape');
-//     }
-
-//     container.classList.add(set.sets.join(''));
-//     const title = document.createElement('h3');
-//     title.textContent = set.content.title;
-//     container.appendChild(title);
-
-//     const ul = document.createElement('ul');
-//     set.content.items.forEach(item => {
-//       const li = document.createElement('li');
-//       li.textContent = item;
-//       ul.appendChild(li);
-//     });
-//     container.appendChild(ul);
-
-//   });
-// };
-// draw(document.querySelector('.venn'), data);
-
 export default function decorate(block) {
   block.querySelectorAll('& > div').forEach((row) => {
     console.log('row', row);
@@ -47,12 +16,26 @@ export default function decorate(block) {
     row.classList.add(sets.join(''));
     row.firstElementChild.remove();
     const title = row.querySelector('h3');
-    const list = row.querySelector('ul');
+    const uls = row.querySelectorAll('ul');
 
-    row.prepend(list);
-    if (title) row.prepend(title);
+    const parents = [];
+    const div = document.createElement('div');
+    div.classList.add('sets');
+    [...uls].forEach((ul) => {
+      parents.push(ul.parentElement);
+      div.append(ul);
+    });
+    row.append(div);
+
+    parents.forEach((p) => p.remove());
+
+    if (title) {
+      const parent = title.parentElement;
+      row.prepend(title);
+      parent.remove();
+    }
+
     row.prepend(document.createElement('span'));
     row.prepend(document.createElement('span'));
-
   });
 }
