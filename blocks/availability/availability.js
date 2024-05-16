d3f062af581466dc4a4457d83ed2c5160589818d
fkakatie <ksexton@adobe.com>
2024-05-16 16:46:24 -0600

    fix: ensure tooltip always displays within the bounds of the containing svg

--- blocks/availability/availability.js
+++ blocks/availability/availability.js
@@ -9,27 +9,87 @@ function generateRandomNumber(max, excluding) {
   return randomIndex;
 }
 
-function buildTooltip() {
-  const tooltip = createTag('div', {
-    'aria-hidden': true,
-    class: 'tooltip',
-    id: 'availability-tooltip',
-  });
-  return tooltip;
+function tooltipAfterOverride(tooltip, axis, pixels) {
+  const block = tooltip.closest('.block');
+  let style = tooltip.closest('.block').querySelector('style');
+  if (axis === 'reset' && !style) return;
+  if (!style) {
+    style = createTag('style');
+    block.append(style);
+  }
+  if (axis === 'reset') style.remove();
+  else if (axis === 'left') {
+    // write tooltip override
+    const op = pixels > 0 ? '-' : '+';
+    // NOTE: 6 is the width of tooltip::after
+    const halfTip = Math.floor(tooltip.clientWidth / 2);
+    const tooltipShift = (op === '+' ? Math.floor(pixels - 6) : Math.ceil(pixels + 6));
+    if (Math.abs(tooltipShift) >= halfTip) {
+      style.textContent = `.availability .tooltip::after { ${axis}: calc(50% ${op} ${halfTip}px); }`;
+      tooltip.classList.add(op === '+' ? 'max' : 'min');
+    } else {
+      style.textContent = `.availability .tooltip::after { ${axis}: calc(50% ${op} ${Math.abs(pixels)}px); }`;
+    }
+  }
+}
+
+function moveTooltip(tooltip, axis, direction) {
+  const style = window.getComputedStyle(tooltip);
+  if (axis === 'x') { // move horizontally
+    tooltip.style.left = `${(parseFloat(style.left, 10) + direction).toFixed(2)}px`;
+    tooltipAfterOverride(tooltip, 'left', direction);
+  } else if (axis === 'y') { // flip vertically
+    tooltip.classList.add('flip');
+    const matrix = new DOMMatrix(window.getComputedStyle(tooltip).transform);
+    tooltip.style.transform = new DOMMatrix([
+      matrix.a,
+      matrix.b,
+      matrix.c,
+      matrix.d,
+      matrix.e,
+      Math.abs(matrix.f / 2.5),
+    ]);
+  }
+}
+
+function ensureTooltipInsideBoundingBox(exterior, interior, tooltip) {
+  // check horizontal bounds
+  const fitsLeft = interior.left >= exterior.left;
+  if (!fitsLeft) moveTooltip(tooltip, 'x', exterior.left - interior.left);
+  const fitsRight = interior.right <= exterior.right;
+  if (!fitsRight) moveTooltip(tooltip, 'x', exterior.right - interior.right);
+  // check vertical bounds
+  const fitsTop = interior.top >= exterior.top;
+  if (!fitsTop) moveTooltip(tooltip, 'y');
+}
+
+function positionTooltip(tooltip, pin, svg) {
+  const width = svg.width.baseVal.value;
+  const height = svg.height.baseVal.value;
+  const cx = parseFloat(pin.getAttribute('cx'), 10);
+  const cy = parseFloat(pin.getAttribute('cy'), 10);
+  tooltip.style.top = `${100 * (cy / height)}%`;
+  tooltip.style.left = `${100 * (cx / width)}%`;
+  const svgRect = svg.getBoundingClientRect();
+  const tipRect = tooltip.getBoundingClientRect();
+  const svgPadding = 6;
+  ensureTooltipInsideBoundingBox({
+    left: svgRect.left + svgPadding,
+    top: svgRect.top + svgPadding,
+    right: svgRect.right - svgPadding,
+  }, tipRect, tooltip);
 }
 
 function focusPin(pin, tooltip, svg) {
   // move focused pin to front
   pin.parentElement.append(pin);
   pin.dataset.focus = true;
-  // rewrite and reposition tooltip
-  const width = svg.width.baseVal.value;
-  const height = svg.height.baseVal.value;
-  const cx = parseFloat(pin.getAttribute('cx'), 10);
-  const cy = parseFloat(pin.getAttribute('cy'), 10);
+  // reset, rewrite, and reposition tooltip
+  tooltip.classList = 'tooltip';
+  tooltip.removeAttribute('style');
+  tooltipAfterOverride(svg.closest('.block'), 'reset');
   tooltip.textContent = pin.getAttribute('aria-label');
-  tooltip.style.top = `${100 * (cy / height)}%`;
-  tooltip.style.left = `${100 * (cx / width)}%`;
+  positionTooltip(tooltip, pin, svg);
   tooltip.setAttribute('aria-hidden', false);
 }
 
@@ -52,6 +112,15 @@ function iterateThroughPins(pins, svg, tooltip, lastIndex = -1) {
   }, 6000);
 }
 
+function buildTooltip() {
+  const tooltip = createTag('div', {
+    'aria-hidden': true,
+    class: 'tooltip',
+    id: 'availability-tooltip',
+  });
+  return tooltip;
+}
+
 async function populateData(src, svg, tooltip) {
   const group = document.createElementNS('http://www.w3.org/2000/svg', 'g');
   group.id = 'availability-group';
@@ -64,22 +133,17 @@ async function populateData(src, svg, tooltip) {
   const { data } = await req.json();
   data.forEach((city) => {
     const pin = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
-    pin.setAttribute('aria-describedby', 'availability-tooltip');
     // position pin on map according to latitude and longitude
-    const cx = (width / 2) + (parseFloat(city.Longitude, 10) * longUnit);
-    const cy = (height / 2) - (parseFloat(city.Latitude, 10) * latUnit);
+    const cx = ((width / 2) + (parseFloat(city.Longitude, 10) * longUnit)).toFixed(2);
+    const cy = ((height / 2) - (parseFloat(city.Latitude, 10) * latUnit)).toFixed(2);
     pin.setAttribute('cy', cy);
     pin.setAttribute('cx', cx);
     pin.setAttribute('r', 33);
     pin.setAttribute('aria-label', city.City);
     group.append(pin);
     // enable pin hover
-    pin.addEventListener('mouseenter', () => {
-      focusPin(pin, tooltip, svg);
-    });
-    pin.addEventListener('mouseleave', () => {
-      unfocusPin(tooltip, svg);
-    });
+    pin.addEventListener('mouseenter', () => focusPin(pin, tooltip, svg));
+    pin.addEventListener('mouseleave', () => unfocusPin(tooltip, svg));
     svg.addEventListener('mouseenter', () => {
       svg.dataset.auto = false;
     });
