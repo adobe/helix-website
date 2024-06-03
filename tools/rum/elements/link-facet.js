40fecbd1fabd9601093fd174992994ca14680bdd
Lars Trieloff <lars@trieloff.net>
2024-06-03 18:36:34 +0200

    feat(rum-explorer): turn on thumbnails for URL facet

--- tools/rum/elements/link-facet.js
+++ tools/rum/elements/link-facet.js
@@ -10,6 +10,14 @@ import ListFacet from './list-facet.js';
 export default class LinkFacet extends ListFacet {
   // eslint-disable-next-line class-methods-use-this
   createLabelHTML(labelText) {
+    const thumbnailAtt = this.getAttribute('thumbnail');
+    if (thumbnailAtt && labelText.startsWith('https://')) {
+      const u = new URL('https://www.aem.live/tools/rum/_ogimage');
+      u.searchParams.set('proxyurl', labelText);
+      return `
+      <img src="${u.href}" title="${labelText}" alt="thumbnail image for ${labelText}" onerror="this.classList.add('broken')">
+      <a href="${labelText}" target="_new">${labelText}</a>`;
+    }
     if (labelText.startsWith('https://') || labelText.startsWith('http://')) {
       return `<a href="${labelText}" target="_new">${labelText}</a>`;
     }
