646064b48ae4b12f19174c217d9d8e8d545c9788
Alexandre Capt <acapt@adobe.com>
2024-06-04 15:27:13 +0200

    feat: level handling

--- blocks/venn/venn.js
+++ blocks/venn/venn.js
@@ -6,4 +6,31 @@ export default async function decorate(block) {
     const layout = segments[i];
     if (layout) segment.className = `venn-content-${layout}`;
   });
+
+  // retrieve level query param
+  const urlParams = new URLSearchParams(window.location.search);
+  const level = urlParams.get('level') || 1;
+  content.querySelectorAll('li').forEach((li) => {
+    const str = li.textContent;
+    // extract start and end levels from string - format: (1,6)
+
+    const match = str.match(/\((\d+),(\d+)\)/);
+    if (match) {
+      const [start, end] = match.slice(1).map(Number);
+      if (level >= start) {
+        li.classList.add('venn-active');
+      }
+      // remove levels from string
+      const a = li.querySelector('a');
+      if (a) {
+        li.lastChild.textContent = li.lastChild.textContent.replace(match[0], '');
+      } else {
+        li.textContent = li.textContent.replace(match[0], '');
+      }
+
+      const p = ((level - start) / (end - start)) * 100;
+
+      li.style.background = `linear-gradient(90deg, rgba(0,255,0,1) 0%, rgba(255,255,255,1) ${p}%)`;
+    }
+  });
 }
