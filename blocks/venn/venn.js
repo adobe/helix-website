82bb8839f57c542eb16bb9a649572dd5eece1a80
fkakatie <ksexton@adobe.com>
2024-06-05 14:34:22 -0600

    chore: lint

--- blocks/venn/venn.js
+++ blocks/venn/venn.js
@@ -1,3 +1,39 @@
+import { createTag } from '../../scripts/scripts.js';
+
+/**
+ * Updates the 'level' query parameter in the URL.
+ * @param {number} value Value for the 'level' parameter.
+ */
+function updateLevelParam(value) {
+  const params = new URLSearchParams(window.location.search);
+  params.set('level', value);
+  const url = `${window.location.pathname}?${params.toString()}`;
+  window.history.replaceState({ path: url }, '', url);
+}
+
+/**
+ * Updates the venn content display based on the value of the range.
+ * @param {HTMLInputElement} range Range input element.
+ * @param {number} value Value of the range.
+ * @param {NodeListOf<HTMLLIElement>} lis List of <li> elements to update.
+ */
+function updateVennDisplay(range, value, lis) {
+  range.value = value;
+  lis.forEach((li) => {
+    li.removeAttribute('style');
+    const min = parseInt(li.dataset.min, 10);
+    const max = parseInt(li.dataset.max, 10);
+    if (value < min) {
+      li.className = 'below-range';
+    } else if (value >= min && value < max) {
+      li.className = 'in-range';
+      li.style.setProperty('--venn-progress', `${parseInt((value / max) * 100, 10)}%`);
+    } else {
+      li.className = 'exceeds-range';
+    }
+  });
+}
+
 export default async function decorate(block) {
   const content = block.firstElementChild;
   content.className = 'venn-content';
@@ -6,4 +42,49 @@ export default async function decorate(block) {
     const layout = segments[i];
     if (layout) segment.className = `venn-content-${layout}`;
   });
+
+  // establish skill level ranges
+  let sliderMin = 0;
+  let sliderMax = 0;
+  const lis = block.querySelectorAll('li');
+  lis.forEach((wrapper) => {
+    // identify skill from <a> tag
+    const skill = wrapper.querySelector('a');
+    // separate skill level range
+    const levels = wrapper.textContent.replace(skill.textContent, '').trim();
+    wrapper.innerHTML = skill.outerHTML;
+    if (levels) {
+      // parse min and max from skill level range
+      const [min, max] = levels.replace('(', '').split(',').map((n) => parseInt(n, 10));
+      if (sliderMin === 0 || min < sliderMin) sliderMin = min;
+      if (sliderMax === 0 || max > sliderMax) sliderMax = max;
+      wrapper.dataset.min = min;
+      wrapper.dataset.max = max;
+    }
+  });
+
+  // build skill slider
+  const slider = createTag('div', { class: 'venn-slider' });
+  const range = createTag('input', { type: 'range', name: 'venn-slider', step: 1 });
+  // set range min and max based on skill level values
+  range.min = sliderMin;
+  range.max = sliderMax;
+  range.addEventListener('input', () => {
+    updateLevelParam(range.value);
+    updateVennDisplay(range, parseInt(range.value, 10), lis);
+  });
+  const label = createTag('label', { type: 'range', for: 'venn-slider' });
+  label.textContent = 'Skill Level';
+  const ticks = createTag('div', { class: 'venn-ticks' });
+  // TODO: remove hard-coded text
+  ticks.innerHTML = `<span>Beginner</span>
+    <span>Expert</span>`;
+  slider.append(label, range, ticks);
+  block.prepend(slider);
+
+  // retrieve level query param
+  const params = new URLSearchParams(window.location.search);
+  const level = params.get('level') || sliderMin;
+  updateLevelParam(level);
+  updateVennDisplay(range, level, lis);
 }
