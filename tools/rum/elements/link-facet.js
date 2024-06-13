0f4b6759a69374ffb34b67c0e7b34c549006f6ee
Lars Trieloff <lars@trieloff.net>
2024-06-13 11:26:38 +0200

    feat(rum-explorer): hide protocol and hostname from link facet if there is not enough space

--- tools/rum/elements/link-facet.js
+++ tools/rum/elements/link-facet.js
@@ -1,6 +1,12 @@
 import { escapeHTML } from '../utils.js';
 import ListFacet from './list-facet.js';
 
+function labelURLParts(url) {
+  const u = new URL(url);
+  return ['protocol', 'hostname', 'port', 'pathname', 'search', 'hash']
+    .reduce((acc, part) => `${acc}<span class="${part}" title="${u.href}">${u[part]}</span>`, '');
+}
+
 /**
  * A custom HTML element to display a list of facets with links.
  * <link-facet facet="userAgent" drilldown="share.html" mode="all">
@@ -18,7 +24,7 @@ export default class LinkFacet extends ListFacet {
       u.searchParams.set('proxyurl', labelText);
       return `
       <img loading="lazy" src="${u.href}" title="${labelText}" alt="thumbnail image for ${labelText}" onerror="this.classList.add('broken')">
-      <a href="${labelText}" target="_new">${labelText}</a>
+      <a href="${labelText}" target="_new">${labelURLParts(labelText)}</a>
       <a href="${pagespeedAtt}${encodeURIComponent(labelText)}" target="_new" class="icon pagespeed" title="Show pagespeed insights for ${labelText}">pagespeed</a>`;
     }
     if (thumbnailAtt && (labelText.startsWith('http://') || labelText.startsWith('https://') || labelText.startsWith('android-app://'))) {
@@ -26,7 +32,7 @@ export default class LinkFacet extends ListFacet {
       u.searchParams.set('proxyurl', labelText);
       return `
       <img loading="lazy" src="${u.href}" title="${labelText}" alt="thumbnail image for ${labelText}" onerror="${faviconAtt ? `this.src='https://www.google.com/s2/favicons?domain=${labelText}&sz=256';this.classList.add('favicon');` : 'this.classList.add(\'broken\');'}">
-      <a href="${labelText}" target="_new">${labelText}</a>`;
+      <a href="${labelText}" target="_new">${labelURLParts(labelText)}</a>`;
     }
     if (labelText.startsWith('https://') || labelText.startsWith('http://')) {
       return `<a href="${labelText}" target="_new">${labelText}</a>`;
