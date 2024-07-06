62c766c38353686ff41dd2f40eff4f506f847ee5
Xinyi Feng <xfeng@adobe.com>
2024-07-05 18:16:56 -0700

    add document eventlistner for properly decorate fragments

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -561,6 +561,26 @@ function buildAuthorBox(main) {
   main.append(div);
 }
 
+// Properly decorate fragments that were pulled in
+document.addEventListener('aem:experimentation', (ev) => {
+  // Do not redecorate the default content
+  if (ev.detail.variant === 'control' || ev.detail?.campaign === 'default' || ev.detail?.audience === 'default') {
+    return;
+  }
+  // Rebuild the autoblock as needed
+  if (ev.detail.element.classList.contains('hero')) {
+    const parent = ev.detail.element.parentElement.parentElement;
+    [...ev.detail.element.children].reverse().forEach((el) => parent.prepend(el));
+    ev.detail.element.remove();
+    decorateBlocks(parent);
+    loadBlocks(parent);
+  } else if (ev.detail.element.classList.contains('block')) {
+    // Otherwise, just reset the replaced blocks and redecorate them
+    decorateBlock(ev.detail.element);
+    loadBlock(ev.detail.element);
+  }
+});
+
 // --------------- Main functions here ---------------- //
 
 /**
