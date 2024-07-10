537ca5d5fee4a2a63d27320c4d341a2b267e7c42
Lars Trieloff <lars@trieloff.net>
2024-07-10 13:29:33 -0700

    fix(number-format): update format from content, not title

--- tools/oversight/elements/number-format.js
+++ tools/oversight/elements/number-format.js
@@ -7,7 +7,9 @@ export default class NumberFormat extends HTMLElement {
   }
 
   connectedCallback() {
-    this.mutationObserver = new MutationObserver(() => this.updateState());
+    this.mutationObserver = new MutationObserver(() => {
+      this.updateState();
+    });
     this.updateState();
     this.mutationObserver.observe(this, { childList: true });
   }
@@ -15,7 +17,7 @@ export default class NumberFormat extends HTMLElement {
   updateState() {
     // stop observing while updating
     this.mutationObserver.disconnect();
-    const number = parseFloat(this.getAttribute('title')) || parseFloat(this.textContent, 10);
+    const number = parseFloat(this.textContent, 10);
     const sampleSize = parseInt(this.getAttribute('sample-size'), 10);
     const total = parseInt(this.getAttribute('total'), 10);
     const precision = parseInt(this.getAttribute('precision'), 10);
