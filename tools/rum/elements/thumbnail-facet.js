618a70ed6255dff4e453fc888b1c5ce381800833
Alexandre Capt <acapt@adobe.com>
2024-08-08 11:30:33 +0200

    feat: remove image thumbnail

--- tools/rum/elements/thumbnail-facet.js
+++ tools/rum/elements/thumbnail-facet.js
@@ -15,8 +15,15 @@ export default class ThumbnailFacet extends ListFacet {
   // eslint-disable-next-line class-methods-use-this
   createLabelHTML(labelText) {
     const fileName = labelText.split('/').pop().replace(/\?.*/, '');
-    if (labelText.startsWith('https://') && labelText.includes('media_')) {
-      return `<img src="${labelText}?width=750&format=webply&optimize=medium" title="${escapeHTML(labelText)}"><span class="filename">${escapeHTML(fileName)}</span>`;
+    const isMediaStr = labelText.startsWith('media_');
+    if ((labelText.startsWith('https://') && labelText.includes('media_')) || isMediaStr) {
+      let src = labelText;
+      if (isMediaStr) {
+        const domain = new URL(window.location.href).searchParams.get('domain');
+        // cannot work in all cases (context path...), but good enough for now
+        src = `https://${domain}/${labelText}`;
+      }
+      return `<img src="${src}?width=750&format=webply&optimize=medium" title="${escapeHTML(labelText)}"><span class="filename">${escapeHTML(fileName)}</span>`;
     } if (labelText.startsWith('http') && labelText.match(/\.(jpeg|jpg|gif|png|svg|webp)$/)) {
       return `<img src="${labelText}" title="${escapeHTML(labelText)}"><span class="filename">${escapeHTML(fileName)}</span>`;
     }
