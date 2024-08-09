003a613dd9ec005f40acfc1a3f768a1681cbd519
Karl Pauls <kpauls@adobe.com>
2024-08-09 16:22:45 +0200

    feat(rum-explorer): split draw from loadData

--- tools/oversight/slicer.js
+++ tools/oversight/slicer.js
@@ -344,8 +344,6 @@ async function loadData(scope, chart) {
   } else if (scope === 'year') {
     dataChunks.load(await loader.fetchPrevious12Months(endDate));
   }
-
-  draw();
 }
 
 export function updateState() {
@@ -411,7 +409,7 @@ const io = new IntersectionObserver((entries) => {
     elems.incognito.addEventListener('change', async () => {
       loader.domainKey = elems.incognito.getAttribute('domainkey');
       await loadData(view, herochart);
-      herochart.draw();
+      draw();
     });
 
     herochart.render();
@@ -425,7 +423,7 @@ const io = new IntersectionObserver((entries) => {
     elems.timezoneElement.textContent = timezone;
 
     if (elems.incognito.getAttribute('domainkey')) {
-      loadData(view, herochart);
+      loadData(view, herochart).then(draw);
     }
 
     elems.filterInput.addEventListener('input', () => {
