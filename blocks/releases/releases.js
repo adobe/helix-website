90897e5e1a610136d4ba0f32d12b41b849b9aa6b
rofe <github@rofe.com>
2024-09-12 21:30:05 +0200

    chore: switch to helix-config-service

--- blocks/releases/releases.js
+++ blocks/releases/releases.js
@@ -15,7 +15,7 @@ function mdToHTML(md) {
 const displayNames = {
   'helix-pipeline-service': 'Delivery Pipeline',
   'helix-admin': 'Admin API',
-  'helix-config': 'Config Service',
+  'helix-config-service': 'Configuration Service',
   'helix-importer-ui': 'Content Importer',
   'helix-cli': 'Command Line Interface',
   'helix-sidekick-extension': 'Sidekick Extension',
