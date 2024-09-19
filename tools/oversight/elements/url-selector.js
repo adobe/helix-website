84faacac4f532077bc13a60cb18c59dcd1b90b15
Max Edell <mgedell@gmail.com>
2024-09-19 09:47:05 +0200

    feat: get domain suggestions for url input (#653)
    * feat: get domain suggestions for url input
    
    * fix: dont use this
    
    * fix: make suggestions lazier

--- tools/oversight/elements/url-selector.js
+++ tools/oversight/elements/url-selector.js
@@ -28,19 +28,23 @@ export default class URLSelector extends HTMLElement {
         }
       </style>
       <label for="url"><img src="https://www.aem.live/favicon.ico"></label>
-      <input id="url" type="url">
+      <input id="url" type="url" list="rum-domain-suggestions">
+      <datalist id="rum-domain-suggestions"></datalist>
     `;
   }
 
   connectedCallback() {
     this.innerHTML = this.template;
+    const datalist = this.querySelector('datalist');
     const input = this.querySelector('input');
     input.value = new URL(window.location.href).searchParams.get('domain');
     const img = this.querySelector('img');
     img.src = `https://www.google.com/s2/favicons?domain=${input.value.split(':')[0]}&sz=64`;
 
-    if (!getPersistentToken()) {
+    const token = getPersistentToken();
+    if (!token) {
       input.disabled = true;
+      datalist.remove();
 
       // detect a click with shift key pressed
       img.addEventListener('click', (event) => {
@@ -50,6 +54,26 @@ export default class URLSelector extends HTMLElement {
           targetlocation.searchParams.set('returnTo', window.location.href);
           window.location.href = targetlocation.href;
         }
+      });
+    } else {
+      fetch('https://rum.fastly-aem.page/domains?suggested=true', {
+        headers: {
+          accept: 'application/json',
+          authorization: `Bearer ${token}`,
+        },
+      }).then(async (resp) => {
+        if (!resp.ok) {
+          datalist.remove();
+        } else {
+          const { domains } = await resp.json();
+          domains.forEach((domain) => {
+            const option = document.createElement('option');
+            option.value = domain;
+            datalist.appendChild(option);
+          });
+        }
+      }).catch(() => {
+        datalist.remove();
       });
     }
 
