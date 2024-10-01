a7fb76bd15d81b2247da3faab1025431f053b178
Alexandre Capt <acapt@adobe.com>
2024-10-01 10:28:50 +0200

    feat(rum-oversight): date range picker

--- tools/oversight/loader.js
+++ tools/oversight/loader.js
@@ -55,7 +55,19 @@ export default class DataLoader {
     return u.toString();
   }
 
-  async fetchUTCMonth(utcISOString) {
+  // eslint-disable-next-line class-methods-use-this
+  filterByDateRange(data, start, end) {
+    if (start || end) {
+      const filtered = data.filter((bundle) => {
+        const time = new Date(bundle.timeSlot);
+        return ((start ? time >= start : true) && (end ? time <= end : true));
+      });
+      return filtered;
+    }
+    return data;
+  }
+
+  async fetchUTCMonth(utcISOString, start, end) {
     this.granularity = 'month';
     const [date] = utcISOString.split('T');
     const dateSplits = date.split('-');
@@ -66,10 +78,10 @@ export default class DataLoader {
     const json = await resp.json();
     const { rumBundles } = json;
     rumBundles.forEach((bundle) => addCalculatedProps(bundle));
-    return { date, rumBundles };
+    return { date, rumBundles: this.filterByDateRange(rumBundles, start, end) };
   }
 
-  async fetchUTCDay(utcISOString) {
+  async fetchUTCDay(utcISOString, start, end) {
     this.granularity = 'day';
     const [date] = utcISOString.split('T');
     const datePath = date.split('-').join('/');
@@ -78,10 +90,10 @@ export default class DataLoader {
     const json = await resp.json();
     const { rumBundles } = json;
     rumBundles.forEach((bundle) => addCalculatedProps(bundle));
-    return { date, rumBundles };
+    return { date, rumBundles: this.filterByDateRange(rumBundles, start, end) };
   }
 
-  async fetchUTCHour(utcISOString) {
+  async fetchUTCHour(utcISOString, start, end) {
     this.granularity = 'hour';
     const [date, time] = utcISOString.split('T');
     const datePath = date.split('-').join('/');
@@ -91,11 +103,11 @@ export default class DataLoader {
     const json = await resp.json();
     const { rumBundles } = json;
     rumBundles.forEach((bundle) => addCalculatedProps(bundle));
-    return { date, hour, rumBundles };
+    return { date, hour, rumBundles: this.filterByDateRange(rumBundles, start, end) };
   }
 
-  async fetchLastWeek() {
-    const date = new Date();
+  async fetchLastWeek(endDate) {
+    const date = endDate ? new Date(endDate) : new Date();
     const hoursInWeek = 7 * 24;
     const promises = [];
     for (let i = 0; i < hoursInWeek; i += 1) {
@@ -120,7 +132,7 @@ export default class DataLoader {
 
   async fetchPrevious12Months(endDate) {
     const date = endDate ? new Date(endDate) : new Date();
-    const months = 12;
+    const months = 13; // 13 to include 2 partial months (first and last)
     const promises = [];
     for (let i = 0; i < months; i += 1) {
       promises.push(this.fetchUTCMonth(date.toISOString()));
@@ -161,6 +173,46 @@ export default class DataLoader {
         return date.toISOString();
       }).map((month) => this.fetchUTCMonth(month));
     }
+    return Promise.all(promises);
+  }
+
+  async fetchPeriod(startDate, endDate) {
+    const start = new Date(startDate);
+    const originalStart = new Date(start);
+    const end = endDate ? new Date(endDate) : new Date();
+
+    const diff = end.getTime() - start.getTime();
+    if (diff < 0) {
+      throw new Error('Start date must be before end date');
+    }
+
+    const promises = [];
+
+    if (diff <= (1000 * 60 * 60 * 24 * 7)) {
+      // less than a week
+      const days = Math.round((diff / (1000 * 60 * 60 * 24))) + 1;
+
+      for (let i = 0; i < days; i += 1) {
+        promises.push(this.fetchUTCDay(start.toISOString(), originalStart, end));
+        start.setDate(start.getDate() + 1);
+      }
+    } else if (diff <= (1000 * 60 * 60 * 24 * 31)) {
+      // less than a month
+      const days = Math.round((diff / (1000 * 60 * 60 * 24))) + 1;
+
+      for (let i = 0; i < days; i += 1) {
+        promises.push(this.fetchUTCDay(start.toISOString(), originalStart, end));
+        start.setDate(start.getDate() + 1);
+      }
+    } else {
+      const months = Math.round(diff / (1000 * 60 * 60 * 24 * 31)) + 1;
+
+      for (let i = 0; i < months; i += 1) {
+        promises.push(this.fetchUTCMonth(start.toISOString(), originalStart, end));
+        start.setMonth(start.getMonth() + 1);
+      }
+    }
+
     return Promise.all(promises);
   }
 }
